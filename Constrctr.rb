c822ea25304dbbeaf7a4e9fa01ee30be4c9f2572
muhammedshameem33 <shameempp33@gmail.com>
2020-08-12 13:45:12 +0530

    [muhammed shameem] add constrct.rb

---
+++ Constrctr.rb
@@ -0,0 +1,9 @@
+class ConstructorDemo
+  #parameter less ConstructorDemo
+  def initialize
+    puts "object created successfully"
+  end
+end
+
+ #invoke default constructor
+obj=ConstructorDemo.new
