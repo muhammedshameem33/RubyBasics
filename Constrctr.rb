98f783f6f5e29209fb1e5c33c44969160a1703fb
muhammedshameem33 <shameempp33@gmail.com>
2020-08-12 14:43:08 +0530

    [muhammed shameem] modify constrct.rb with constructor overloading

--- Constrctr.rb
+++ Constrctr.rb
@@ -1,8 +1,9 @@
 class ConstructorDemo
   @length
   @breadth
-  #initialize the instanse variables
-  def initialize(length,breadth)
+  #To achieve constructor overloading initialize values to 0
+  def initialize(length=0,breadth=0)
+    puts "object created"
     @length=length
     @breadth=breadth
   end
@@ -11,4 +12,8 @@ end
 
 #invoke constructor with parameter
 obj=ConstructorDemo.new(5,2)
-puts obj.instance_variable_get(:@length)*obj.instance_variable_get(:@breadth)
+#instance variable can be accessed through methods
+area= obj.instance_variable_get(:@length)*obj.instance_variable_get(:@breadth)
+puts "area = #{area}"
+#invoke default constructor
+obj2=ConstructorDemo.new
