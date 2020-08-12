1af087dd2781faecec3cc8027056482d93e1b75a
muhammedshameem33 <shameempp33@gmail.com>
2020-08-12 14:18:53 +0530

    [muhammed shameem] modify constrct.rb with parameter constructor

--- Constrctr.rb
+++ Constrctr.rb
@@ -1,9 +1,14 @@
 class ConstructorDemo
-  #parameter less ConstructorDemo
-  def initialize
-    puts "object created successfully"
+  @length
+  @breadth
+  #initialize the instanse variables
+  def initialize(length,breadth)
+    @length=length
+    @breadth=breadth
   end
+
 end
 
- #invoke default constructor
-obj=ConstructorDemo.new
+#invoke constructor with parameter
+obj=ConstructorDemo.new(5,2)
+puts obj.instance_variable_get(:@length)*obj.instance_variable_get(:@breadth)
